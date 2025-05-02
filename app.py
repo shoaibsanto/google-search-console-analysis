a5bb2b787fa1e52cdecedbb8aa59270c82f9467b
Shoaib Santo <44254821+shoaibsanto@users.noreply.github.com>
2025-05-02 12:55:05 +0600

    app.py

--- app.py
+++ app.py
@@ -348,28 +348,6 @@ def main():
     min_position_quick = st.sidebar.slider("Minimum Position for Quick Wins", 5, 20, 11)
     max_position_quick = st.sidebar.slider("Maximum Position for Quick Wins", 20, 50, 20)
     
-    st.sidebar.markdown("---")
-    st.sidebar.subheader("⭐ For Power Members")
-
-    # Checkbox for keyword metrics
-    include_metrics = st.sidebar.checkbox("Include additional keyword metrics")
-    
-    api_key = None
-    user_id = None
-    country_code = None
-    if include_metrics:
-        api_key = st.sidebar.text_input("Enter your API Key", type="password")
-        user_id = st.sidebar.text_input("Enter your User ID")
-        country_options = {
-            "United States": "US","United Kingdom": "UK","India": "IN", "Argentina": "AR", "Australia": "AU", "Brazil": "BR", "Canada": "CA", 
-            "Germany": "DE", "Spain": "ES", "France": "FR", "Ireland": "IE", 
-             "Italy": "IT", "Mexico": "MX", "Netherlands": "NL", 
-            "New Zealand": "NZ", "Singapore": "SG", "Ukraine": "UA", 
-             "South Africa": "ZA"
-        }
-        country_name = st.sidebar.selectbox("Select Country", list(country_options.keys()))
-        country_code = country_options[country_name]
-    
     # Button to start analysis
     start_analysis = st.button("Start Analysis")
     
